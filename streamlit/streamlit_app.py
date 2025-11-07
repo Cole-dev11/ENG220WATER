a89d031fbe32953817a091c815f131c2ae263d4d
rickwgarcia <rickwltrgarcia@gmail.com>
2025-11-07 10:40:56 -0700

    fix hilight error

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -73,8 +73,7 @@ map_data = st_folium(
     m,
     use_container_width=True,
     height=500,
-    returned_objects=["last_object_clicked"],
-    add_marker_on_click=False
+    returned_objects=["last_object_clicked"]
 )
 
 # --- 5. Handle Click Data ---
