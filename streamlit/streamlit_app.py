0f54e14ed0bcfb16e1f211f1735147c809d9bb84
rickwgarcia <rickwltrgarcia@gmail.com>
2025-11-07 10:52:53 -0700

    removed redudancy

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -67,26 +67,17 @@ folium.GeoJson(
     )
 ).add_to(m)
 
-# --- 4. Display Map and Capture Clicks ---
-st.write("Click on a county to see its name below.")
-map_data = st_folium(
+# --- 4. Display Map ---
+# st.write("Click on a county to see its name below.") # <-- REMOVED THIS LINE
+st_folium(
     m,
     use_container_width=True,
-    height=500,
-    returned_objects=["last_geojson_clicked"]  # <-- FIX 1
+    height=500
+    # returned_objects=["last_geojson_clicked"]  # <-- REMOVED THIS PARAMETER
 )
 
 # --- 5. Handle Click Data ---
-st.header("Click Information")
-
-if map_data.get("last_geojson_clicked"):  # <-- FIX 2 (Part A)
-    try:
-        county_name = map_data["last_geojson_clicked"]["NAMELSAD10"]  # <-- FIX 2 (Part B)
-        st.success(f"You clicked on **{county_name}**!")
-    except KeyError:
-        st.warning("Clicked on the map, but couldn't find the county name property.")
-else:
-    st.info("No county clicked yet.")
+# <-- THIS ENTIRE SECTION HAS BEEN REMOVED -->
 
 
 # --- 6. Other Page Components ---
